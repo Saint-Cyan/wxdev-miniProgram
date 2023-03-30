45f414f84656c01c5583239c70f722847a008872
Cyan <1736967686@qq.com>
2023-03-30 12:36:55 +0800

    xyx-3.30-delete image

--- router/image/image.js
+++ router/image/image.js
@@ -2,8 +2,3 @@
 const express = require('express')
 //引入router
 const router = express.Router()
-
-router.get('/:imageName'(req,res)=>{
-	const imageName = req.params.imageName
-	return res.sendFile("http://localhost:7788/public/" + imageName)
-})
